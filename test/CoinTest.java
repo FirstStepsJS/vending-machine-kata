c5a2df3d696a249c87e56b574abc1efafa384fa6
Yasmin Rodriguez <yasmin.rodriguez@gmail.com>
2017-12-03 18:54:02 -0500

    Completed test for when a coin object is checked against a null coin object

--- test/CoinTest.java
+++ test/CoinTest.java
@@ -1,14 +1,22 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class CoinTest {
 	@Test
-	public void equals_SameCoinObject_ReturnsTrue() {
+	public void equals_WhenComparingToSameCoinObject_ReturnsTrue() {
 		Coin coin = new Coin(1, 1, 1, 1);
 		Coin coin2 = coin;
 
 		assertTrue(coin.equals(coin2));
 	}
 
+	@Test
+	public void equals_WhenComparingToNullCoinObject_ReturnsFalse() {
+		Coin coin = new Coin(1, 1, 1, 1);
+		Coin coin2 = null;
+
+		assertFalse(coin.equals(coin2));
+	}
 }
